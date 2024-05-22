8e20a082348a7c0d707486838f3c9b9482fd5bf0
aleperfect <143966062+aleperfect@users.noreply.github.com>
2024-05-22 18:03:16 -0500

    a

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -4,17 +4,17 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `*🛍️https://chat.whatsapp.com/CCDGO1lIuH3Ggr4nA44cbY* ${pesan}`
-let teks = `*⬇️Venta de 🤖 ,regedits 🖥️📱 ,sensibilidades📱 ,+ 30 metodos ,cuentas ff y mass⬇️*
- ${oi}\n\n🛍️ *@Ale.izn_20:*\n`
+let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:ACTIVENSE‼️* ${pesan}`
+let teks = `🛍️ *𝑽𝒆𝒏𝒕𝒂𝒔 𝑩𝒖𝒏𝒏𝒚*
+https://chat.whatsapp.com/HRiBA6p8H3D5ZHcargNsvD\n\n🟩 ${oi}\n\n🟩 *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
 for (let mem of participants) {
-teks += `⭐ @${mem.id.split('@')[0]}\n`}
-teks += `➤ 𝐑𝐞𝐚𝐝𝐳 𝐀𝐥𝐞𝐢𝐳𝐧`
+teks += `🐰💜 @${mem.id.split('@')[0]}\n`}
+teks += `𝐁𝐮𝐧𝐧𝐲 𝐁𝐨𝐭`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
 handler.tags = ['group']
-handler.command = /^(tagall|invocar|invocacion|todos|invocación|aviso|despierten)$/i
+handler.command = /^(tagall|invocar|bunny|todos|invocación)$/i
 handler.admin = true
 handler.group = true
 export default handler
